2739782e8704a8e198849bcf2bb332135965b53d
queenyoon123 <queenyoon123@naver.com>
2026-03-26 16:22:49 +0900

    first commit

---
+++ src/main/java/com/example/ssafyspringstudy/SsafySpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.ssafyspringstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsafySpringStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsafySpringStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ssafyspringstudy/SsafySpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ssafyspringstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsafySpringStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
