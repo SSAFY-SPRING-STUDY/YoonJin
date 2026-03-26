9bff5f1941684f5817e04d67ffef5ee6a7461b4a
queenyoon123 <queenyoon123@naver.com>
2026-03-26 16:27:31 +0900

    pra

---
+++ src/main/java/com/example/ssafyspringstudy/controller/HelloController.java
@@ -0,0 +1,27 @@
+package com.example.ssafyspringstudy.controller;
+
+import com.example.ssafyspringstudy.service.HelloService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+    private final HelloService helloService;
+
+    @Autowired
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
+
+    @GetMapping("/hello")
+    public String hello(){
+        String str = helloService.getString();
+        return str;
+    }
+
+
+
+}
